df6f2d3362077c958344b78e436c1a330467c8df
jananga99 <jananga1999@gmail.com>
2024-04-18 00:12:40 +0530

    Global db get all metadata bug fixed

--- db/global_db.py
+++ db/global_db.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 from validators.validators import validate_metadata, validate_metadata_id
 from bson import ObjectId
 from formatters.db import (
-    from_db_metadata,
+    from_global_db_metadata,
 )
 
 global_db_url = os.getenv("GLOBAL_DB_URL")
@@ -16,7 +16,7 @@ global_metadata = global_db["metadata"]
 def insert_metadata(metadata):
     validate_metadata(metadata, with_id=False)
     global_metadata.insert_one(metadata)
-    metadata = from_db_metadata(metadata)
+    metadata = from_global_db_metadata(metadata)
     validate_metadata(metadata)
     return metadata
 
@@ -26,14 +26,14 @@ def get_one_metadata(id):
     metadata = global_metadata.find_one({"_id": ObjectId(id)})
     if metadata is None:
         raise Error(f"Metadata with id: {id} not found", 404)
-    metadata = from_db_metadata(metadata)
+    metadata = from_global_db_metadata(metadata)
     validate_metadata(metadata)
     return metadata
 
 
 def get_all_metadata():
     all_metadata = list(global_metadata.find({}))
-    for metadata in all_metadata:
-        metadata = from_db_metadata(metadata)
-        validate_metadata(metadata)
+    for i in range(len(all_metadata)):
+        all_metadata[i] = from_global_db_metadata(all_metadata[i])
+        validate_metadata(all_metadata[i])
     return all_metadata
--- formatters/db.py
+++ formatters/db.py
@@ -16,6 +16,21 @@ def from_db_metadata(metadata):
     }
 
 
+def from_global_db_metadata(metadata):
+    return {
+        "id": str(metadata["_id"]),
+        "name": metadata["name"],
+        "size": metadata["size"],
+        "type": metadata["type"],
+        "access_type": metadata["access_type"],
+        "merkel_root": metadata["merkel_root"],
+        "start_chunk_id": metadata["start_chunk_id"],
+        "start_chunk_node_id": metadata["start_chunk_node_id"],
+        "created_at": metadata["created_at"],
+        "lastViewed_at": metadata["lastViewed_at"],
+    }
+
+
 def to_db_metadata(metadata):
     if "id" in metadata:
         metadata["_id"] = metadata["id"]
